0ae06554daefc16b03cf827cec0ca1f650fca11d
alvinkenyagah <alvin47k@gmail.com>
2023-05-25 19:27:59 +0300

    add sql queries

--- sql.sql
+++ sql.sql
@@ -1,9 +1,11 @@
-SELECT * FROM cats;
+-- SELECT * FROM cats;
 
-SELECT AVG(net_worth) FROM cats;
+-- SELECT AVG(net_worth) FROM cats;
 
-SELECT SUM(net_worth) FROM cats;
+-- SELECT SUM(net_worth) FROM cats;
 
-SELECT MIN(net_worth) FROM cats;
+-- SELECT MIN(net_worth) FROM cats;
 
-SELECT MAX(net_worth) FROM cats;
+-- SELECT MAX(net_worth) FROM cats;
+
+SELECT * FROM cats ORDER BY net_worth DESC LIMIT 2;
